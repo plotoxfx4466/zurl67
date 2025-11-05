03c7d22c34dcb2aaf412d050d8498c813fe8ccac
plotoxfx4466 <sibelsaglambb@gmail.com>
2025-11-05 21:55:09 +0300

    zurl
    yok

---
+++ zurl.py
@@ -0,0 +1,16 @@
+import time
+import webbrowser
+
+#---------------------------
+time.sleep(25 * 60)
+
+webbrowser.open("https://www.youtube.com/watch?v=G1gJrHyeuZE&list=RDG1gJrHyeuZE&start_radio=1&t=36s")
+#---------------------------
+time.sleep(10 * 60)
+
+webbrowser.open("https://www.youtube.com/watch?v=CWOrDXnnmOc&list=RDG1gJrHyeuZE&index=3&t=21s")
+#---------------------------
+time.sleep(5 * 60)
+
+webbrowser.open("https://www.youtube.com/watch?v=9RLX57BZjQs&list=RDG1gJrHyeuZE&index=7&t=15s")
+
